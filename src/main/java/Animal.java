340e1a72e000bb9ef9fa3bff162db3dd2bddf0aa
Josh O'Brian <joshuaeobrian@gmail.com>
2017-03-03 11:53:02 -0500

    First Commit

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,3 +2,11 @@
  * Create the most minimal class you can. The class must be named "Animal"
  * because it must match the file name.
  */
+public class Animal{
+
+		public String animalType(){
+			return "I am a lion";
+		}
+
+
+}
